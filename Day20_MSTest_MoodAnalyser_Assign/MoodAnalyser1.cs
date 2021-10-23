7ea8952d823dd8a6b9621572931e613ec767e2bc
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-23 08:24:25 +0530

    [Kavita] added MoodAnalyser Program

---
+++ Day20_MSTest_MoodAnalyser_Assign/MoodAnalyser1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_MSTest_MoodAnalyser_Assign
+{
+    public class MoodAnalyser1
+    {
+        private string message;
+        public MoodAnalyser1(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser1()
+        {
+        }
+        public string analyseMood()
+        {
+            //"null"==""
+            // string s = null;
+            //string abc = string.Empty;          //professional definition
+
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+
+
+
+}
